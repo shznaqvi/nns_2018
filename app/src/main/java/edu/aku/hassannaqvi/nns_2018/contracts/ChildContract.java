48286b5697a6783b370981d94c9917592cdc3aa6
javed.khan <javedak09@gmail.com>
2018-02-07 15:24:10 +0500

    Initial Commit

--- app/src/main/java/edu/aku/hassannaqvi/nns_2018/contracts/ChildContract.java
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/contracts/ChildContract.java
@@ -12,6 +12,7 @@ import org.json.JSONObject;
 
 public class ChildContract {
 
+
     private final String projectName = "DMU-TOICSCREENING";
     //private final String surveyType = "SN";
     private String _ID = "";
