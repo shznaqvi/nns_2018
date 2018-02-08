f1b90835eeaf7ef0f279601e55a63b5f9056d6c9
ramsha <ramshasaeed2016@gmail.com>
2018-02-08 12:11:54 +0500

    Section A8 layout

--- app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionA8Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionA8Activity.java
@@ -2,6 +2,7 @@ package edu.aku.hassannaqvi.nns_2018.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SectionA8Activity extends AppCompatActivity {
 
@@ -9,5 +10,30 @@ public class SectionA8Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section_a8);
+    }
+    public void BtnContinue() {
+
+        Toast.makeText(this, "Processing This Section", Toast.LENGTH_SHORT).show();
+        /*if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                Toast.makeText(this, "Starting Ending Section", Toast.LENGTH_SHORT).show();
+
+                finish();
+
+                startActivity(new Intent(this, ChildAssessmentActivity.class));
+
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }*/
+    }
+
+    public void BtnEnd() {
+        MainApp.endActivity(this, this);
     }
 }
