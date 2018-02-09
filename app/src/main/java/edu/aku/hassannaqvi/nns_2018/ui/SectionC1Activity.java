907819f1abb1705ad836595c71810f2e31905a22
ali.azaz <openmind_ali@hotmail.com>
2018-02-09 11:14:29 +0500

    c1 layout complete and sec a4 skip pattern done

---
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionC1Activity.java
@@ -0,0 +1,52 @@
+package edu.aku.hassannaqvi.nns_2018.ui;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import edu.aku.hassannaqvi.nns_2018.core.DatabaseHelper;
+import edu.aku.hassannaqvi.nns_2018.core.MainApp;
+import edu.aku.hassannaqvi.nns_2018.databinding.ActivitySectionC1Binding;
+
+public class SectionC1Activity extends AppCompatActivity {
+
+    ActivitySectionC1Binding binding;
+    DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_section_c1);
+        db = new DatabaseHelper(this);
+
+//        Assigning data to UI binding
+        binding.setCallback(this);
+    }
+
+    public void BtnContinue() {
+
+        Toast.makeText(this, "Processing This Section", Toast.LENGTH_SHORT).show();
+        /*if (formValidation()) {
+            try {
+                SaveDraft();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if (UpdateDB()) {
+                Toast.makeText(this, "Starting Ending Section", Toast.LENGTH_SHORT).show();
+
+                finish();
+
+                startActivity(new Intent(this, ChildAssessmentActivity.class));
+
+            } else {
+                Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
+            }
+        }*/
+    }
+
+    public void BtnEnd() {
+        MainApp.endActivity(this, this);
+    }
+}
