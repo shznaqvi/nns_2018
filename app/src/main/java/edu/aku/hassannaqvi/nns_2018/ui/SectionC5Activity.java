4da7eaa6dd6c8c5f9e87e59f84d495ce8dbd9a09
ramsha <ramshasaeed2016@gmail.com>
2018-02-09 11:10:18 +0500

    Section c5 layout

---
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionC5Activity.java
@@ -0,0 +1,19 @@
+package edu.aku.hassannaqvi.nns_2018.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SectionC5Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_c5);
+    }
+    public void BtnContinue(){
+
+    }
+    public void BtnEnd(){
+
+    }
+}
