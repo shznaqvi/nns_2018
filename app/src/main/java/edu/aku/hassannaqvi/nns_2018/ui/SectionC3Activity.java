47dd9e62df31aabd91a076d2b4d006fc72a9f8a1
ali.azaz <openmind_ali@hotmail.com>
2018-02-10 15:50:11 +0500

    Skip patterns done

--- app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionC3Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionC3Activity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import edu.aku.hassannaqvi.nns_2018.R;
@@ -12,29 +13,63 @@ import edu.aku.hassannaqvi.nns_2018.databinding.ActivitySectionC3Binding;
 
 public class SectionC3Activity extends AppCompatActivity {
 
-    ActivitySectionC3Binding bi;
+    ActivitySectionC3Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_section_c3);
 
-        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_c3);
-        bi.setCallback(this);
-        bi.nc3bcgdt.setManager(getSupportFragmentManager());
-        bi.nc3opv0dt.setManager(getSupportFragmentManager());
-        bi.nc3opv1dt.setManager(getSupportFragmentManager());
-        bi.nc3penta1dt.setManager(getSupportFragmentManager());
-        bi.nc3pcv1dt.setManager(getSupportFragmentManager());
-        bi.nc3opv2dt.setManager(getSupportFragmentManager());
-        bi.nc3p2dt.setManager(getSupportFragmentManager());
-        bi.nc3pcv2dt.setManager(getSupportFragmentManager());
-        bi.nc3opv3dt.setManager(getSupportFragmentManager());
-        bi.nc3p3dt.setManager(getSupportFragmentManager());
-        bi.nc3pcv3dt.setManager(getSupportFragmentManager());
-        bi.nc3ipvdt.setManager(getSupportFragmentManager());
-        bi.nc3m1dt.setManager(getSupportFragmentManager());
-        bi.nc3m2dt.setManager(getSupportFragmentManager());
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_section_c3);
+        binding.setCallback(this);
+        binding.nc3bcgdt.setManager(getSupportFragmentManager());
+        binding.nc3opv0dt.setManager(getSupportFragmentManager());
+        binding.nc3opv1dt.setManager(getSupportFragmentManager());
+        binding.nc3penta1dt.setManager(getSupportFragmentManager());
+        binding.nc3pcv1dt.setManager(getSupportFragmentManager());
+        binding.nc3opv2dt.setManager(getSupportFragmentManager());
+        binding.nc3p2dt.setManager(getSupportFragmentManager());
+        binding.nc3pcv2dt.setManager(getSupportFragmentManager());
+        binding.nc3opv3dt.setManager(getSupportFragmentManager());
+        binding.nc3p3dt.setManager(getSupportFragmentManager());
+        binding.nc3pcv3dt.setManager(getSupportFragmentManager());
+        binding.nc3ipvdt.setManager(getSupportFragmentManager());
+        binding.nc3m1dt.setManager(getSupportFragmentManager());
+        binding.nc3m2dt.setManager(getSupportFragmentManager());
+
+//        Skip Patterns
+        binding.nc310.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == R.id.nc310b) {
+                    binding.nc311.clearCheck();
+                    binding.nc312.clearCheck();
+                    binding.nc313.clearCheck();
+                }
+            }
+        });
+
+        binding.nc305.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == R.id.nc305c) {
+                    binding.nc3bcg.clearCheck();
+                    binding.nc3opv0.clearCheck();
+                    binding.nc3opv1.clearCheck();
+                    binding.nc3penta1.clearCheck();
+                    binding.nc3pcv1.clearCheck();
+                    binding.nc3opv2.clearCheck();
+                    binding.nc3p2.clearCheck();
+                    binding.nc3pcv2.clearCheck();
+                    binding.nc3opv3.clearCheck();
+                    binding.nc3p3.clearCheck();
+                    binding.nc3pcv3.clearCheck();
+                    binding.nc3ipv.clearCheck();
+                    binding.nc3m1.clearCheck();
+                    binding.nc3m2.clearCheck();
+                }
+            }
+        });
 
     }
 
