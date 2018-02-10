7723f0ecdee4dc9f2e67ce6e3e7335a3632455a1
gul.sanober <gulsanobermemon@gmail.com>
2018-02-10 17:48:35 +0500

    Form Validation

--- app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionA2Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionA2Activity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import edu.aku.hassannaqvi.nns_2018.R;
 import edu.aku.hassannaqvi.nns_2018.core.MainApp;
 import edu.aku.hassannaqvi.nns_2018.databinding.ActivitySectionA2Binding;
+import edu.aku.hassannaqvi.nns_2018.validation.validatorClass;
 
 
 public class SectionA2Activity extends AppCompatActivity {
@@ -33,17 +35,23 @@ public class SectionA2Activity extends AppCompatActivity {
     public void setupViews() {
         bi.na2dob.setManager(getSupportFragmentManager());
 
-        bi.na20799.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        bi.na20798.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     bi.na2dob.setEnabled(false);
                     bi.na2dob.setText(null);
-                    bi.na2age.setEnabled(true);
+                    bi.na2aged.setEnabled(true);
+                    bi.na2agem.setEnabled(true);
+                    bi.na2agey.setEnabled(true);
                 } else {
                     bi.na2dob.setEnabled(true);
-                    bi.na2age.setEnabled(false);
-                    bi.na2age.setText(null);
+                    bi.na2aged.setEnabled(false);
+                    bi.na2aged.setText(null);
+                    bi.na2agem.setEnabled(false);
+                    bi.na2agem.setText(null);
+                    bi.na2agey.setEnabled(false);
+                    bi.na2agey.setText(null);
                 }
             }
         });
@@ -56,7 +64,7 @@ public class SectionA2Activity extends AppCompatActivity {
     public void BtnContinue() {
 
         Toast.makeText(this, "Processing This Section", Toast.LENGTH_SHORT).show();
-        /*if (formValidation()) {
+        /*if (ValidateForm()) {
             try {
                 SaveDraft();
             } catch (JSONException e) {
@@ -83,4 +91,106 @@ public class SectionA2Activity extends AppCompatActivity {
         MainApp.endActivity(this, this);
 
     }
+
+    private boolean ValidateForm() {
+
+        Toast.makeText(this, "Validating This Section ", Toast.LENGTH_SHORT).show();
+
+        if (!validatorClass.EmptyTextBox(this, bi.na202, getString(R.string.na202))) {
+            return false;
+        }
+
+        if (!validatorClass.EmptyRadioButton(this, bi.na203, bi.na203a, getString(R.string.na203))) {
+            return false;
+        }
+
+        if (!validatorClass.EmptyRadioButton(this, bi.na203, bi.na20396, bi.na20396x, getString(R.string.na203) + " - " + getString(R.string.other))) {
+            return false;
+        }
+
+
+        if (!validatorClass.EmptySpinner(this, bi.na204, getString(R.string.na204))) {
+            return false;
+        }
+
+        if (!validatorClass.EmptySpinner(this, bi.na205, getString(R.string.na205))) {
+            return false;
+        }
+
+        if (!validatorClass.EmptyRadioButton(this, bi.na206, bi.na206a, getString(R.string.na206))) {
+            return false;
+        }
+
+        if (bi.na20798.isChecked()) {
+
+            if (!validatorClass.EmptyTextBox(this, bi.na2aged, getString(R.string.na2age))) {
+                return false;
+            }
+
+            if (!validatorClass.RangeTextBox(this, bi.na2aged, 0, 29, getString(R.string.na2age), " days")) {
+                return false;
+            }
+
+            if (!validatorClass.EmptyTextBox(this, bi.na2agem, getString(R.string.na2age))) {
+                return false;
+            }
+
+            if (!validatorClass.RangeTextBox(this, bi.na2agem, 0, 11, getString(R.string.na2age), " months")) {
+                return false;
+            }
+
+            if (!validatorClass.EmptyTextBox(this, bi.na2agey, getString(R.string.na2age))) {
+                return false;
+            }
+
+            if (!validatorClass.RangeTextBox(this, bi.na2aged, 0, 99, getString(R.string.na2age), " years")) {
+                return false;
+            }
+
+            if (bi.na2aged.getText().toString().equals("0") && bi.na2agem.getText().toString().equals("0") && bi.na2agey.getText().toString().equals("0")) {
+                Toast.makeText(this, "ERROR(invalid): " + "All can not be zero" + getString(R.string.na2age), Toast.LENGTH_LONG).show();
+                bi.na2agey.setError("All can not be zero");
+                bi.na2agem.setError("All can not be zero");
+                bi.na2aged.setError("All can not be zero");
+                Log.i(SectionA2Activity.class.getSimpleName(), "na2age" + ": This data is Required!");
+            } else {
+                bi.na2agey.setError(null);
+                bi.na2agem.setError(null);
+                bi.na2aged.setError(null);
+            }
+
+
+        } else {
+            if (!validatorClass.EmptyTextBox(this, bi.na2dob, getString(R.string.na2dob))) {
+                return false;
+            }
+
+        }
+
+        if (!validatorClass.EmptyRadioButton(this, bi.na2edu, bi.na2edua, getString(R.string.na2edu))) {
+            return false;
+        }
+
+
+        if (!validatorClass.EmptyRadioButton(this, bi.na2occ, bi.na2occa, getString(R.string.na2occ))) {
+            return false;
+        }
+
+        if (!validatorClass.EmptyRadioButton(this, bi.na2occ, bi.na2occ96, bi.na2occ96x, getString(R.string.na2occ) + " - " + getString(R.string.other))) {
+            return false;
+        }
+
+        if (!validatorClass.EmptyRadioButton(this, bi.na2ms, bi.na2msa, getString(R.string.na2ms))) {
+            return false;
+        }
+
+        if (!validatorClass.EmptyRadioButton(this, bi.na212, bi.na212a, getString(R.string.na212))) {
+            return false;
+        }
+
+        return validatorClass.EmptyTextBox(this, bi.na213, getString(R.string.na213));
+    }
+
+
+
 }
--- app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionA3Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionA3Activity.java
@@ -10,6 +10,7 @@ import edu.aku.hassannaqvi.nns_2018.R;
 import edu.aku.hassannaqvi.nns_2018.core.DatabaseHelper;
 import edu.aku.hassannaqvi.nns_2018.core.MainApp;
 import edu.aku.hassannaqvi.nns_2018.databinding.ActivitySectionA3Binding;
+import edu.aku.hassannaqvi.nns_2018.validation.validatorClass;
 
 public class SectionA3Activity extends AppCompatActivity {
 
@@ -38,7 +39,7 @@ public class SectionA3Activity extends AppCompatActivity {
     public void BtnContinue() {
 
         Toast.makeText(this, "Processing This Section", Toast.LENGTH_SHORT).show();
-        /*if (formValidation()) {
+        /*if (ValidateForm()) {
             try {
                 SaveDraft();
             } catch (JSONException e) {
@@ -64,6 +65,40 @@ public class SectionA3Activity extends AppCompatActivity {
     public void BtnEnd() {
         MainApp.endActivity(this, this);
     }
+
+
+    private boolean ValidateForm() {
+
+        Toast.makeText(this, "Validating This Section ", Toast.LENGTH_SHORT).show();
+
+        if (!validatorClass.EmptyTextBox(this, bi.na3w, getString(R.string.na3w))) {
+            return false;
+        }
+
+        if (!validatorClass.EmptyTextBox(this, bi.na3h, getString(R.string.na3h))) {
+            return false;
+        }
+
+        if (!validatorClass.EmptyTextBox(this, bi.na3muac, getString(R.string.na3muac))) {
+            return false;
+        }
+
+
+        if (!validatorClass.EmptyRadioButton(this, bi.na3bcgscar, bi.na3bcgscara, getString(R.string.na3bcgscar))) {
+            return false;
+        }
+
+        if (!validatorClass.EmptyRadioButton(this, bi.na3g, bi.na3ga, getString(R.string.na3g))) {
+            return false;
+        }
+
+        if (!validatorClass.EmptyRadioButton(this, bi.na3ca, bi.na3caa, getString(R.string.na3ca))) {
+            return false;
+        }
+
+        return validatorClass.EmptyRadioButton(this, bi.na3o, bi.na3oa, getString(R.string.na3o));
+    }
+
 
 
 }
