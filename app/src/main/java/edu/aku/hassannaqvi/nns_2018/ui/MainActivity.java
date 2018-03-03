497a1edd22fba0b39e09902eb5b8962739909c9a
ramsha.ahmed <ramshasaeed2016@gmail.com>
2018-03-03 11:52:51 +0500

    validation modification in activity C3 according to version 2.0

--- app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/MainActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/MainActivity.java
@@ -305,7 +305,7 @@ public class MainActivity extends Activity {
 
 
     public void openA(View v) {
-        Intent iA = new Intent(this, SectionC5Activity.class);
+        Intent iA = new Intent(this, SectionC3Activity.class);
         startActivity(iA);
     }
 
