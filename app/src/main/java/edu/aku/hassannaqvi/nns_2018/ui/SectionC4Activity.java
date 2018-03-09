f78e12e4bfec22bf1c5fd7f50f741ca0784d0dbe
ramsha.ahmed <ramshasaeed2016@gmail.com>
2018-03-09 11:10:53 +0500

    Minor changes and fixed issues after testing

--- app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionC4Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionC4Activity.java
@@ -575,15 +575,15 @@ public class SectionC4Activity extends Activity {
         sC4.put("nc4119603x", binding.nc4119603x.getText().toString());
 
 //     nc412
-        sC4.put("nc412b", binding.nc412b.isChecked() ? "1" : "0");
-        sC4.put("nc412c", binding.nc412c.isChecked() ? "2" : "0");
+        sC4.put("nc412a", binding.nc412b.isChecked() ? "1" : "0");
+        sC4.put("nc412b", binding.nc412c.isChecked() ? "2" : "0");
 //        sC4.put("nc412d", binding.nc412d.isChecked() ? "3" : "0");
-        sC4.put("nc412e", binding.nc412e.isChecked() ? "3" : "0");
-        sC4.put("nc412f", binding.nc412f.isChecked() ? "4" : "0");
-        sC4.put("nc412g", binding.nc412g.isChecked() ? "5" : "0");
-        sC4.put("nc412h", binding.nc412h.isChecked() ? "6" : "0");
-        sC4.put("nc412i", binding.nc412i.isChecked() ? "7" : "0");
-        sC4.put("nc412j", binding.nc412j.isChecked() ? "8" : "0");
+        sC4.put("nc412c", binding.nc412e.isChecked() ? "3" : "0");
+        sC4.put("nc412d", binding.nc412f.isChecked() ? "4" : "0");
+        sC4.put("nc412e", binding.nc412g.isChecked() ? "5" : "0");
+        sC4.put("nc412f", binding.nc412h.isChecked() ? "6" : "0");
+        sC4.put("nc412g", binding.nc412i.isChecked() ? "7" : "0");
+        sC4.put("nc412h", binding.nc412j.isChecked() ? "8" : "0");
         sC4.put("nc4129601", binding.nc4129601.isChecked() ? "96" : "0");
 
         sC4.put("nc4129601x", binding.nc4129601x.getText().toString());
