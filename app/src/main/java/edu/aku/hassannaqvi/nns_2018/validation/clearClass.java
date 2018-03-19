86f1f88ccf18c0207dc2d22f77fa163bf7c7ab97
ali.azaz <openmind_ali@hotmail.com>
2018-03-19 18:28:46 +0500

    clearClass add

---
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/validation/clearClass.java
@@ -0,0 +1,68 @@
+package edu.aku.hassannaqvi.nns_2018.validation;
+
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+/**
+ * Created by ali.azaz on 03/19/18.
+ */
+
+public class clearClass {
+
+    public static void ClearRadioButton(LinearLayout container, RadioGroup rdGrp) {
+        if (rdGrp.getCheckedRadioButtonId() == -1) {
+
+            rdGrp.clearCheck();
+
+            for (int i = 0; i < container.getChildCount(); i++) {
+                View v = container.getChildAt(i);
+                if (v instanceof RadioButton) {
+                    v.setEnabled(false);
+                }
+            }
+        }
+    }
+
+    public static void ClearRadioButton(LinearLayout container, RadioGroup rdGrp, EditText othertxt) {
+        if (rdGrp.getCheckedRadioButtonId() == -1) {
+
+            rdGrp.clearCheck();
+            othertxt.setText(null);
+
+            for (int i = 0; i < container.getChildCount(); i++) {
+                View v = container.getChildAt(i);
+                if (v instanceof RadioButton) {
+                    v.setEnabled(false);
+                }
+            }
+        }
+    }
+
+    public static void ClearCheckBoxes(LinearLayout container) {
+        for (int i = 0; i < container.getChildCount(); i++) {
+            View v = container.getChildAt(i);
+            if (v instanceof CheckBox) {
+                ((CheckBox) v).setChecked(false);
+                v.setEnabled(false);
+            }
+        }
+    }
+
+    public static void ClearCheckBoxes(LinearLayout container, EditText othertxt) {
+
+        othertxt.setText(null);
+
+        for (int i = 0; i < container.getChildCount(); i++) {
+            View v = container.getChildAt(i);
+            if (v instanceof CheckBox) {
+                ((CheckBox) v).setChecked(false);
+                v.setEnabled(false);
+            }
+        }
+    }
+
+}
