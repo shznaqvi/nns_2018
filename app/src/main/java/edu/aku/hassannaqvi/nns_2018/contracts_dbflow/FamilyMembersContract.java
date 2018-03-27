1c5f0a5a730b786fdd8ba71b1716800fdebc05f8
ramsha.ahmed <ramshasaeed2016@gmail.com>
2018-03-27 17:44:54 +0500

    Household Form Contract

--- app/src/main/java/edu/aku/hassannaqvi/nns_2018/contracts_dbflow/FamilyMembersContract.java
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/contracts_dbflow/FamilyMembersContract.java
@@ -18,5 +18,50 @@ public class FamilyMembersContract extends BaseModel {
     @Unique
     public int _id;
 
+    @Column
+    public String projectname = "NNS-2018";
+    @Column
+    private String _uid;
+    @Column
+    private String _uuid;
+    @Column
+    private String formdate;
+    @Column
+    private String user;
+    @Column
+    private String istatus;
+    @Column
+    private String istatus88x;
+    @Column
+    private String enm_no;
+    @Column
+    private String resp_lno;
+    @Column
+    private String hh_no;
+    @Column
+    private String gpslat;
+    @Column
+    private String gpslng;
+    @Column
+    private String gpsdate;
+    @Column
+    private String gpsacc;
+    @Column
+    private String gpselev;
+    @Column
+    private String deviceid;
+    @Column
+    private String devicetagid;
+    @Column
+    private String synced;
+    @Column
+    private String synced_date;
+    @Column
+    private String appversion;
+
+    // JSON
+
+
+
 
 }
