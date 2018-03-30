317ea041ae51a4a337c41bdae0b04f191be1ff49
ali.azaz <openmind_ali@hotmail.com>
2018-03-30 10:49:06 +0500

    c3 model class done

--- app/src/main/java/edu/aku/hassannaqvi/nns_2018/other/JSONC3ModelClass.java
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/other/JSONC3ModelClass.java
@@ -9,73 +9,36 @@ public class JSONC3ModelClass {
     private String nc302 = "";
     private String nc303 = "";
     private String nc3bcg = "";
-    private String nc3bcgd = "";
-    private String nc3bcgm = "";
-    private String nc3bcgy = "";
+    private String nc3bcgsrc = "";
     private String nc3opv0 = "";
-    private String nc3opv0d = "";
-    private String nc3opv0m = "";
-    private String nc3opv0y = "";
+    private String nc3opv0src = "";
     private String nc3opv1 = "";
-    private String nc3opv1d = "";
-    private String nc3opv1m = "";
-    private String nc3opv1y = "";
+    private String nc3opv1src = "";
     private String nc3p1 = "";
-    private String nc3p1d = "";
-    private String nc3p1m = "";
-    private String nc3p1y = "";
+    private String nc3p1src = "";
     private String nc3pcv1 = "";
-    private String nc3pcv1d = "";
-    private String nc3pcv1m = "";
-    private String nc3pcv1y = "";
+    private String nc3pcv1src = "";
     private String nc3opv2 = "";
-    private String nc3opv2d = "";
-    private String nc3opv2m = "";
-    private String nc3opv2y = "";
+    private String nc3opv2src = "";
     private String nc3p2 = "";
-    private String nc3p2d = "";
-    private String nc3p2m = "";
-    private String nc3p2y = "";
+    private String nc3p2src = "";
     private String nc3pcv2 = "";
-    private String nc3pcv2d = "";
-    private String nc3pcv2m = "";
-    private String nc3pcv2y = "";
+    private String nc3pcv2src = "";
     private String nc3opv3 = "";
-    private String nc3opv3d = "";
-    private String nc3opv3m = "";
-    private String nc3opv3y = "";
+    private String nc3opv3src = "";
     private String nc3p3 = "";
-    private String nc3p3d = "";
-    private String nc3p3m = "";
-    private String nc3p3y = "";
+    private String nc3p3src = "";
     private String nc3pcv3 = "";
-    private String nc3pcv3d = "";
-    private String nc3pcv3m = "";
-    private String nc3pcv3y = "";
+    private String nc3pcv3src = "";
     private String nc3ipv = "";
-    private String nc3ipvd = "";
-    private String nc3ipvm = "";
-    private String nc3ipvy = "";
+    private String nc3ipvsrc = "";
     private String nc3m1 = "";
-    private String nc3m1d = "";
-    private String nc3m1m = "";
-    private String nc3m1y = "";
+    private String nc3m1src = "";
     private String nc3m2 = "";
-    private String nc3m2d = "";
-    private String nc3m2m = "";
-    private String nc3m2y = "";
+    private String nc3m2src = "";
     private String nc305 = "";
     private String nc306 = "";
-    private String nc307 = "";
-    private String nc308 = "";
-    private String nc309 = "";
-    private String nc310 = "";
-    private String nc311 = "";
-    private String nc312 = "";
-    private String nc313 = "";
-    private String nc314 = "";
-    private String nc305 = "";
-    private String nc306 = "";
+
 
     public JSONC3ModelClass() {
 
@@ -105,28 +68,12 @@ public class JSONC3ModelClass {
         this.nc3bcg = nc3bcg;
     }
 
-    public String getnc3bcgd() {
-        return nc3bcgd;
+    public String getnc3bcgsrc() {
+        return nc3bcgsrc;
     }
 
-    public void setnc3bcgd(String nc3bcgd) {
-        this.nc3bcgd = nc3bcgd;
-    }
-
-    public String getnc3bcgm() {
-        return nc3bcgm;
-    }
-
-    public void setnc3bcgm(String nc3bcgm) {
-        this.nc3bcgm = nc3bcgm;
-    }
-
-    public String getnc3bcgy() {
-        return nc3bcgy;
-    }
-
-    public void setnc3bcgy(String nc3bcgy) {
-        this.nc3bcgy = nc3bcgy;
+    public void setnc3bcgsrc(String nc3bcgsrc) {
+        this.nc3bcgsrc = nc3bcgsrc;
     }
 
     public String getnc3opv0() {
@@ -137,28 +84,12 @@ public class JSONC3ModelClass {
         this.nc3opv0 = nc3opv0;
     }
 
-    public String getnc3opv0d() {
-        return nc3opv0d;
+    public String getnc3opv0src() {
+        return nc3opv0src;
     }
 
-    public void setnc3opv0d(String nc3opv0d) {
-        this.nc3opv0d = nc3opv0d;
-    }
-
-    public String getnc3opv0m() {
-        return nc3opv0m;
-    }
-
-    public void setnc3opv0m(String nc3opv0m) {
-        this.nc3opv0m = nc3opv0m;
-    }
-
-    public String getnc3opv0y() {
-        return nc3opv0y;
-    }
-
-    public void setnc3opv0y(String nc3opv0y) {
-        this.nc3opv0y = nc3opv0y;
+    public void setnc3opv0src(String nc3opv0src) {
+        this.nc3opv0src = nc3opv0src;
     }
 
     public String getnc3opv1() {
@@ -169,28 +100,12 @@ public class JSONC3ModelClass {
         this.nc3opv1 = nc3opv1;
     }
 
-    public String getnc3opv1d() {
-        return nc3opv1d;
+    public String getnc3opv1src() {
+        return nc3opv1src;
     }
 
-    public void setnc3opv1d(String nc3opv1d) {
-        this.nc3opv1d = nc3opv1d;
-    }
-
-    public String getnc3opv1m() {
-        return nc3opv1m;
-    }
-
-    public void setnc3opv1m(String nc3opv1m) {
-        this.nc3opv1m = nc3opv1m;
-    }
-
-    public String getnc3opv1y() {
-        return nc3opv1y;
-    }
-
-    public void setnc3opv1y(String nc3opv1y) {
-        this.nc3opv1y = nc3opv1y;
+    public void setnc3opv1src(String nc3opv1src) {
+        this.nc3opv1src = nc3opv1src;
     }
 
     public String getnc3p1() {
@@ -201,28 +116,12 @@ public class JSONC3ModelClass {
         this.nc3p1 = nc3p1;
     }
 
-    public String getnc3p1d() {
-        return nc3p1d;
+    public String getnc3p1src() {
+        return nc3p1src;
     }
 
-    public void setnc3p1d(String nc3p1d) {
-        this.nc3p1d = nc3p1d;
-    }
-
-    public String getnc3p1m() {
-        return nc3p1m;
-    }
-
-    public void setnc3p1m(String nc3p1m) {
-        this.nc3p1m = nc3p1m;
-    }
-
-    public String getnc3p1y() {
-        return nc3p1y;
-    }
-
-    public void setnc3p1y(String nc3p1y) {
-        this.nc3p1y = nc3p1y;
+    public void setnc3p1src(String nc3p1src) {
+        this.nc3p1src = nc3p1src;
     }
 
     public String getnc3pcv1() {
@@ -233,28 +132,12 @@ public class JSONC3ModelClass {
         this.nc3pcv1 = nc3pcv1;
     }
 
-    public String getnc3pcv1d() {
-        return nc3pcv1d;
+    public String getnc3pcv1src() {
+        return nc3pcv1src;
     }
 
-    public void setnc3pcv1d(String nc3pcv1d) {
-        this.nc3pcv1d = nc3pcv1d;
-    }
-
-    public String getnc3pcv1m() {
-        return nc3pcv1m;
-    }
-
-    public void setnc3pcv1m(String nc3pcv1m) {
-        this.nc3pcv1m = nc3pcv1m;
-    }
-
-    public String getnc3pcv1y() {
-        return nc3pcv1y;
-    }
-
-    public void setnc3pcv1y(String nc3pcv1y) {
-        this.nc3pcv1y = nc3pcv1y;
+    public void setnc3pcv1src(String nc3pcv1src) {
+        this.nc3pcv1src = nc3pcv1src;
     }
 
     public String getnc3opv2() {
@@ -265,28 +148,12 @@ public class JSONC3ModelClass {
         this.nc3opv2 = nc3opv2;
     }
 
-    public String getnc3opv2d() {
-        return nc3opv2d;
+    public String getnc3opv2src() {
+        return nc3opv2src;
     }
 
-    public void setnc3opv2d(String nc3opv2d) {
-        this.nc3opv2d = nc3opv2d;
-    }
-
-    public String getnc3opv2m() {
-        return nc3opv2m;
-    }
-
-    public void setnc3opv2m(String nc3opv2m) {
-        this.nc3opv2m = nc3opv2m;
-    }
-
-    public String getnc3opv2y() {
-        return nc3opv2y;
-    }
-
-    public void setnc3opv2y(String nc3opv2y) {
-        this.nc3opv2y = nc3opv2y;
+    public void setnc3opv2src(String nc3opv2src) {
+        this.nc3opv2src = nc3opv2src;
     }
 
     public String getnc3p2() {
@@ -297,28 +164,12 @@ public class JSONC3ModelClass {
         this.nc3p2 = nc3p2;
     }
 
-    public String getnc3p2d() {
-        return nc3p2d;
+    public String getnc3p2src() {
+        return nc3p2src;
     }
 
-    public void setnc3p2d(String nc3p2d) {
-        this.nc3p2d = nc3p2d;
-    }
-
-    public String getnc3p2m() {
-        return nc3p2m;
-    }
-
-    public void setnc3p2m(String nc3p2m) {
-        this.nc3p2m = nc3p2m;
-    }
-
-    public String getnc3p2y() {
-        return nc3p2y;
-    }
-
-    public void setnc3p2y(String nc3p2y) {
-        this.nc3p2y = nc3p2y;
+    public void setnc3p2src(String nc3p2src) {
+        this.nc3p2src = nc3p2src;
     }
 
     public String getnc3pcv2() {
@@ -329,28 +180,12 @@ public class JSONC3ModelClass {
         this.nc3pcv2 = nc3pcv2;
     }
 
-    public String getnc3pcv2d() {
-        return nc3pcv2d;
+    public String getnc3pcv2src() {
+        return nc3pcv2src;
     }
 
-    public void setnc3pcv2d(String nc3pcv2d) {
-        this.nc3pcv2d = nc3pcv2d;
-    }
-
-    public String getnc3pcv2m() {
-        return nc3pcv2m;
-    }
-
-    public void setnc3pcv2m(String nc3pcv2m) {
-        this.nc3pcv2m = nc3pcv2m;
-    }
-
-    public String getnc3pcv2y() {
-        return nc3pcv2y;
-    }
-
-    public void setnc3pcv2y(String nc3pcv2y) {
-        this.nc3pcv2y = nc3pcv2y;
+    public void setnc3pcv2src(String nc3pcv2src) {
+        this.nc3pcv2src = nc3pcv2src;
     }
 
     public String getnc3opv3() {
@@ -361,28 +196,12 @@ public class JSONC3ModelClass {
         this.nc3opv3 = nc3opv3;
     }
 
-    public String getnc3opv3d() {
-        return nc3opv3d;
+    public String getnc3opv3src() {
+        return nc3opv3src;
     }
 
-    public void setnc3opv3d(String nc3opv3d) {
-        this.nc3opv3d = nc3opv3d;
-    }
-
-    public String getnc3opv3m() {
-        return nc3opv3m;
-    }
-
-    public void setnc3opv3m(String nc3opv3m) {
-        this.nc3opv3m = nc3opv3m;
-    }
-
-    public String getnc3opv3y() {
-        return nc3opv3y;
-    }
-
-    public void setnc3opv3y(String nc3opv3y) {
-        this.nc3opv3y = nc3opv3y;
+    public void setnc3opv3src(String nc3opv3src) {
+        this.nc3opv3src = nc3opv3src;
     }
 
     public String getnc3p3() {
@@ -393,28 +212,12 @@ public class JSONC3ModelClass {
         this.nc3p3 = nc3p3;
     }
 
-    public String getnc3p3d() {
-        return nc3p3d;
+    public String getnc3p3src() {
+        return nc3p3src;
     }
 
-    public void setnc3p3d(String nc3p3d) {
-        this.nc3p3d = nc3p3d;
-    }
-
-    public String getnc3p3m() {
-        return nc3p3m;
-    }
-
-    public void setnc3p3m(String nc3p3m) {
-        this.nc3p3m = nc3p3m;
-    }
-
-    public String getnc3p3y() {
-        return nc3p3y;
-    }
-
-    public void setnc3p3y(String nc3p3y) {
-        this.nc3p3y = nc3p3y;
+    public void setnc3p3src(String nc3p3src) {
+        this.nc3p3src = nc3p3src;
     }
 
     public String getnc3pcv3() {
@@ -425,28 +228,12 @@ public class JSONC3ModelClass {
         this.nc3pcv3 = nc3pcv3;
     }
 
-    public String getnc3pcv3d() {
-        return nc3pcv3d;
+    public String getnc3pcv3src() {
+        return nc3pcv3src;
     }
 
-    public void setnc3pcv3d(String nc3pcv3d) {
-        this.nc3pcv3d = nc3pcv3d;
-    }
-
-    public String getnc3pcv3m() {
-        return nc3pcv3m;
-    }
-
-    public void setnc3pcv3m(String nc3pcv3m) {
-        this.nc3pcv3m = nc3pcv3m;
-    }
-
-    public String getnc3pcv3y() {
-        return nc3pcv3y;
-    }
-
-    public void setnc3pcv3y(String nc3pcv3y) {
-        this.nc3pcv3y = nc3pcv3y;
+    public void setnc3pcv3src(String nc3pcv3src) {
+        this.nc3pcv3src = nc3pcv3src;
     }
 
     public String getnc3ipv() {
@@ -457,28 +244,12 @@ public class JSONC3ModelClass {
         this.nc3ipv = nc3ipv;
     }
 
-    public String getnc3ipvd() {
-        return nc3ipvd;
+    public String getnc3ipvsrc() {
+        return nc3ipvsrc;
     }
 
-    public void setnc3ipvd(String nc3ipvd) {
-        this.nc3ipvd = nc3ipvd;
-    }
-
-    public String getnc3ipvm() {
-        return nc3ipvm;
-    }
-
-    public void setnc3ipvm(String nc3ipvm) {
-        this.nc3ipvm = nc3ipvm;
-    }
-
-    public String getnc3ipvy() {
-        return nc3ipvy;
-    }
-
-    public void setnc3ipvy(String nc3ipvy) {
-        this.nc3ipvy = nc3ipvy;
+    public void setnc3ipvsrc(String nc3ipvsrc) {
+        this.nc3ipvsrc = nc3ipvsrc;
     }
 
     public String getnc3m1() {
@@ -489,28 +260,12 @@ public class JSONC3ModelClass {
         this.nc3m1 = nc3m1;
     }
 
-    public String getnc3m1d() {
-        return nc3m1d;
+    public String getnc3m1src() {
+        return nc3m1src;
     }
 
-    public void setnc3m1d(String nc3m1d) {
-        this.nc3m1d = nc3m1d;
-    }
-
-    public String getnc3m1m() {
-        return nc3m1m;
-    }
-
-    public void setnc3m1m(String nc3m1m) {
-        this.nc3m1m = nc3m1m;
-    }
-
-    public String getnc3m1y() {
-        return nc3m1y;
-    }
-
-    public void setnc3m1y(String nc3m1y) {
-        this.nc3m1y = nc3m1y;
+    public void setnc3m1src(String nc3m1src) {
+        this.nc3m1src = nc3m1src;
     }
 
     public String getnc3m2() {
@@ -521,108 +276,12 @@ public class JSONC3ModelClass {
         this.nc3m2 = nc3m2;
     }
 
-    public String getnc3m2d() {
-        return nc3m2d;
+    public String getnc3m2src() {
+        return nc3m2src;
     }
 
-    public void setnc3m2d(String nc3m2d) {
-        this.nc3m2d = nc3m2d;
-    }
-
-    public String getnc3m2m() {
-        return nc3m2m;
-    }
-
-    public void setnc3m2m(String nc3m2m) {
-        this.nc3m2m = nc3m2m;
-    }
-
-    public String getnc3m2y() {
-        return nc3m2y;
-    }
-
-    public void setnc3m2y(String nc3m2y) {
-        this.nc3m2y = nc3m2y;
-    }
-
-    public String getnc305() {
-        return nc305;
-    }
-
-    public void setnc305(String nc305) {
-        this.nc305 = nc305;
-    }
-
-    public String getnc306() {
-        return nc306;
-    }
-
-    public void setnc306(String nc306) {
-        this.nc306 = nc306;
-    }
-
-    public String getnc307() {
-        return nc307;
-    }
-
-    public void setnc307(String nc307) {
-        this.nc307 = nc307;
-    }
-
-    public String getnc308() {
-        return nc308;
-    }
-
-    public void setnc308(String nc308) {
-        this.nc308 = nc308;
-    }
-
-    public String getnc309() {
-        return nc309;
-    }
-
-    public void setnc309(String nc309) {
-        this.nc309 = nc309;
-    }
-
-    public String getnc310() {
-        return nc310;
-    }
-
-    public void setnc310(String nc310) {
-        this.nc310 = nc310;
-    }
-
-    public String getnc311() {
-        return nc311;
-    }
-
-    public void setnc311(String nc311) {
-        this.nc311 = nc311;
-    }
-
-    public String getnc312() {
-        return nc312;
-    }
-
-    public void setnc312(String nc312) {
-        this.nc312 = nc312;
-    }
-
-    public String getnc313() {
-        return nc313;
-    }
-
-    public void setnc313(String nc313) {
-        this.nc313 = nc313;
-    }
-
-    public String getnc314() {
-        return nc314;
-    }
-
-    public void setnc314(String nc314) {
-        this.nc314 = nc314;
+    public void setnc3m2src(String nc3m2src) {
+        this.nc3m2src = nc3m2src;
     }
 
     public String getnc305() {
