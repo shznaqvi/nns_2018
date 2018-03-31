4a988da3b57f4c7037765e58a76da15259397961
ali.azaz <openmind_ali@hotmail.com>
2018-03-31 14:05:44 +0500

    changes in viewMem

--- app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/ViewMemberActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/ViewMemberActivity.java
@@ -51,6 +51,13 @@ public class ViewMemberActivity extends AppCompatActivity {
         db = new DatabaseHelper(this);
         binding.setCallback(this);
 
+        if (getIntent().getBooleanExtra("flagEdit", true)) {
+
+            binding.fldGrpEditHH.setVisibility(View.GONE);
+        } else {
+            binding.fldGrpEditHH.setVisibility(View.VISIBLE);
+        }
+
     }
 
     private void initializingLists() {
@@ -242,7 +249,7 @@ public class ViewMemberActivity extends AppCompatActivity {
 //              Set Recycler View
                     wraAdapter = new WraAdapter(MainApp.mwra);
                     if (wraAdapter.getItemCount() != 0) {
-                        binding.nowrafound.setVisibility(View.GONE);
+                        binding.nowrafound.setVisibility(View.INVISIBLE);
                         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
                         binding.recyclerMwra.setLayoutManager(mLayoutManager);
                         binding.recyclerMwra.setItemAnimator(new DefaultItemAnimator());
@@ -340,7 +347,7 @@ public class ViewMemberActivity extends AppCompatActivity {
 
 
                     if (childAdapter.getItemCount() != 0) {
-                        binding.nochildfound.setVisibility(View.GONE);
+                        binding.nochildfound.setVisibility(View.INVISIBLE);
                         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
                         binding.recyclerChild.setLayoutManager(mLayoutManager);
                         binding.recyclerChild.setItemAnimator(new DefaultItemAnimator());
