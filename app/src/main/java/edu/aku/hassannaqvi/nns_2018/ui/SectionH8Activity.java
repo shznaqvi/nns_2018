6810d971de58bcc66a53597069b76218b0609e9b
aliazaz <openmind_Ali@hotmail.com>
2018-04-15 17:38:12 +0500

    new bug fixed

--- app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionH8Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/nns_2018/ui/SectionH8Activity.java
@@ -370,7 +370,7 @@ public class SectionH8Activity extends AppCompatActivity implements TextWatcher,
             sA2.put("nh804", bi.nh804.getSelectedItem().toString());
             sA2.put("nh805", bi.nh805.getSelectedItem().toString());
 
-            sA2.put("mwraSerial", mothersSerials.get(bi.nh804.getSelectedItemPosition()));
+            sA2.put("mwraSerial", mothersMap.get(bi.nh804.getSelectedItemPosition()));
 
         } else {
             sA2.put("edit_updatedate_nh8", new SimpleDateFormat("dd-MM-yyyy HH:mm").format(System.currentTimeMillis()));
